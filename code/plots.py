93a71b004b85d8e0161d645aa26665a2f6486317
Berwin Lan <blan@olin.edu>
2024-04-25 19:11:43 -0400

    Fix overloading

--- code/plots.py
+++ code/plots.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 # Number of infected squads vs. time
 def num_infected(
-    random_walk_path: str, random_waypoint_path: str, hierarchial_path: str
+    random_walk_path: str, random_waypoint_path: str, hierarchical_path: str=None
 ):
     """
     Args:
@@ -31,60 +31,29 @@ def num_infected(
         grouped["tick"], grouped["infected"], ":", color="gray", linewidth=3
     )
 
-    # Plot hierarchical
-    df = pd.read_csv(hierarchial_path)
+    if hierarchical_path:
+        # Plot hierarchical
+        df = pd.read_csv(hierarchical_path)
 
-    # Sum `infected` Trues on each tick
-    grouped = df.groupby("tick").sum().reset_index()
-    plt.plot(
-        grouped["tick"], grouped["infected"], "-", color="blue", linewidth=3
-    )
+        # Sum `infected` Trues on each tick
+        grouped = df.groupby("tick").sum().reset_index()
+        plt.plot(
+            grouped["tick"], grouped["infected"], "-", color="tab:blue", linewidth=3
+        )
 
     # Style plots
     plt.title("Spread of Malware over Time")
     plt.xlabel("Ticks")
     plt.ylabel("# of infected squads")
-    plt.legend(["Random walk", "Random waypoint", "Hierarchical"])
-    plt.show()
-
-
-# Number of infected squads vs. time
-def num_infected(
-    random_walk_path: str, random_waypoint_path: str
-):
-    """
-    Args:
-        agent_log_path: Path to the file created by the agent_logger.
-    """
-    # Plot random walk
-    df = pd.read_csv(random_walk_path)
-
-    # Sum `infected` Trues on each tick
-    grouped = df.groupby("tick").sum().reset_index()
-    plt.plot(
-        grouped["tick"], grouped["infected"], "--", color="orange", linewidth=3
-    )
-
-    # Plot random waypoint
-    df = pd.read_csv(random_waypoint_path)
-
-    # Sum `infected` Trues on each tick
-    grouped = df.groupby("tick").sum().reset_index()
-    plt.plot(
-        grouped["tick"], grouped["infected"], ":", color="gray", linewidth=3
-    )
-
-
-    # Style plots
-    plt.title("Spread of Malware over Time")
-    # plt.xlim([-100, 10000])
-    plt.xlabel("Ticks")
-    plt.ylabel("# of infected squads")
-    plt.legend(["Random walk", "Random waypoint"])
+    if hierarchical_path:
+        plt.legend(["Random walk", "Random waypoint", "Hierarchical"])
+    else:
+        plt.legend(["Random walk", "Random waypoint"])
     plt.show()
 
 
 if __name__ == "__main__":
     random_walk = "out/agent_log_15.csv"
     random_waypoint = "out/agent_log_16.csv"
-    num_infected(random_walk, random_waypoint)
+    hierarchical = "out/agent_log_17.csv"
+    num_infected(random_walk, random_waypoint, hierarchical)
